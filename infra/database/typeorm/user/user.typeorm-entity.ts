d6fd1afff311d38a58c57779d637342b6d39e0e4
Micael Fone <micaelfone0@gmail.com>
2026-01-05 21:40:37 -0300

    chore: remove .vscode, .husky, .editorconfig e .prettierrc do controle de versão

--- infra/database/typeorm/user/user.typeorm-entity.ts
+++ infra/database/typeorm/user/user.typeorm-entity.ts
@@ -1,24 +1,33 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { UserTypeTypeOrmEntity } from '../user/userType.typeorm-entity';
+
 @Entity('users')
-export class UserEntity {
+export class UserTypeOrmEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column({ nullable: false, unique: true })
   name: string;
+
   @Column({ nullable: false, unique: true })
   email: string;
+
   @Column({ nullable: false })
   password: string;
+
   @Column({ nullable: false })
   phoneNumber: string;
+
   @Column({ nullable: false })
   isactive: boolean;
-  @Column({ nullable: false })
-  @ManyToOne(() => UserTypeTypeOrmEntity)
-  userTypeId: number;
+
+  @ManyToOne(() => UserTypeTypeOrmEntity, (userType) => userType.id)
+  @JoinColumn({ name: 'userType' })
+  userType: number;
+
   @Column({ nullable: false })
   createdAt: Date;
+
   @Column({ nullable: true })
   updatedAt: Date | null;
 }
